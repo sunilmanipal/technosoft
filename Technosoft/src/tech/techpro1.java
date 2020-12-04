ab67a8e3973b1d3823db38722db9349e3a99d9bf
Administrator <Administrator@IBM-DOTNET-001>
2020-12-04 16:56:11 +0530

    new code

---
+++ Technosoft/src/tech/techpro1.java
@@ -0,0 +1,12 @@
+package tech;
+
+public class techpro1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my first program in java");
+
+	}
+
+}
